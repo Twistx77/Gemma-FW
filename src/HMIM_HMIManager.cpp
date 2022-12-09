6b74a71d7ee8ef0c26da0dc6130822712f61800e
Alex Mendez <twistx77@gmail.com>
2022-12-09 14:22:25 +0100

    Updated HMI Manager

--- src/HMIM_HMIManager.cpp
+++ src/HMIM_HMIManager.cpp
@@ -10,11 +10,11 @@
 
 #define DEBOUNCE_TIME 50 // TODO: REPLACE CONFIG MANAGER
 
-#define LONG_TOUCH_DURATION_MS 1000       // TODO: REPLACE CONFIG MANAGER
+#define LONG_TOUCH_DURATION_MS 1200       // TODO: REPLACE CONFIG MANAGER
 #define TOUCHPAD_FILTER_TOUCH_PERIOD (10) // TODO: REPLACE CONFIG MANAGER
 #define TOUCH_THRESH_NO_USE (0)
-#define TOUCH_THRESHOLD_PROPORTION (0.7)
-#define NEW_THRESHOLD_PROPORTION (0.05)
+#define TOUCH_THRESHOLD_PROPORTION (0.8)
+#define NEW_THRESHOLD_PROPORTION (0.001)
 #define CURRENT_THRESHOLD_PROPORTION (1 - NEW_THRESHOLD_PROPORTION)
 
 // TODO: Check if this is actually needed
@@ -36,7 +36,7 @@ TouchSensor_t touchSensorTypes[] = {TS_CENTER, TS_LEFT, TS_RIGHT};  // TODO: Rem
 
 static const touch_pad_t sensorsInput[] = {TOUCH_PAD_NUM7, TOUCH_PAD_NUM2, TOUCH_PAD_NUM0}; // CENTER - LEFT - RIGHT
 
-static uint16_t touchThresholds[MAX_TOUCH_SENSORS] = {0xFFFF, 0xFFFF, 0xFFFF}; // Started at Max to be adjusted after booting
+static float touchThresholds[MAX_TOUCH_SENSORS] = {0xFFFF, 0xFFFF, 0xFFFF}; // Started at Max to be adjusted after booting
 
 AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(ROTARY_ENCODER_A_PIN, ROTARY_ENCODER_B_PIN, ROTARY_ENCODER_BUTTON_PIN, -1, ROTARY_ENCODER_STEPS);
 
@@ -73,48 +73,6 @@ void clickHandler(TouchSensor_t &sensor)
         break;
     }
 }
-/*
-void longClickDetectedHandler(TouchSensor_t &sensor)
-{
-    DEBUG_OK("Long Press %s Button Started", SENSOR_NAMES[sensor]);
-    switch (sensor)
-    {
-    case TS_CENTER:
-        // buttonLongPressed[CENTER_TS] = true;
-        break;
-    case TS_LEFT:
-        break;
-    case TS_RIGHT:
-        break;
-    default:
-        DEBUG_ERROR("Unknown button ID %d", sensor);
-        break;
-    }
-}
-
-void longClickHandler(TouchSensor_t &sensor)
-{
-    DEBUG_OK("Long Press %s Button Ended", SENSOR_NAMES[sensor]);
-
-    switch (sensor)
-    {
-    case TS_CENTER:
-        // buttonLongPressed[CENTER_TS] = false;
-        // WST_ToggleIncreaseBrightness(STRIP_CENTER);
-        break;
-    case TS_LEFT:
-        buttonLongPressed[LEFT_TS] = false;
-        MWST_ToggleIncreaseBrightness(STRIP_LEFT);
-        break;
-    case TS_RIGHT:
-        buttonLongPressed[RIGHT_TS] = false;
-        MWST_ToggleIncreaseBrightness(STRIP_RIGHT);
-        break;
-    default:
-        DEBUG_ERROR("Unknown button ID %d", sensor);
-        break;
-    }
-}*/
 
 void IRAM_ATTR readEncoderISR()
 {
@@ -123,33 +81,25 @@ void IRAM_ATTR readEncoderISR()
 
 TouchState_T readTouchSensorState(uint8_t sensor)
 {
-    uint16_t sample;
-    uint16_t touchValue = 0;
+    uint16_t touchValue;
+    touch_pad_read_raw_data(sensorsInput[sensor], &touchValue);
 
-    // touch_pad_read_filtered(sensorInput, &touchValue);
-    for (uint8_t i = 0; i < 5; i++)
-    {
-        // touch_pad_read_filtered(sensorsInput[sensor], &sample);
-        touch_pad_read_raw_data(sensorsInput[sensor], &sample);
-        touchValue += sample;
-    }
 
-     touchThresholds[sensor] = touchThresholds[sensor]* CURRENT_THRESHOLD_PROPORTION + touchValue * TOUCH_THRESHOLD_PROPORTION*NEW_THRESHOLD_PROPORTION;
+    // touchThresholds[sensor] = touchThresholds[sensor]* CURRENT_THRESHOLD_PROPORTION + touchValue * TOUCH_THRESHOLD_PROPORTION*NEW_THRESHOLD_PROPORTION;
 
     if (touchValue < touchThresholds[sensor])
     {
 
-        //Serial.println("1," + String(sensorsInput[sensor]) + "," + String(touchValue) + "," + String(touchThresholds[sensor]));
-        SerialBT.println("1," + String(sensorsInput[sensor]) + "," + String(touchValue) + "," + String(touchThresholds[sensor]));
+        /*     if (sensor == 0)
+             SerialBT.println("1," + String(sensorsInput[sensor]) + "," + String(touchValue) + "," + String(touchThresholds[sensor]));*/
 
         return (TOUCHED);
     }
     else
     {
-       
-        SerialBT.println ("0," + String(sensorsInput[sensor]) + "," + String(touchValue) + "," + String(touchThresholds[sensor]));
-        //Serial.println("0," + String(sensorsInput[sensor]) + "," + String(touchValue) + "," + String(touchThresholds[sensor]));
-        // touchThresholds[sensor] = touchThresholds[sensor] * CURRENT_THRESHOLD_PROPORTION +  touchValue * TOUCH_THRESHOLD_PROPORTION * NEW_THRESHOLD_PROPORTION;
+        /*
+       if (sensor == 0)
+        SerialBT.println ("0," + String(sensorsInput[sensor]) + "," + String(touchValue) + "," + String(touchThresholds[sensor]));*/
         return (NOT_TOUCHED);
     }
 }
@@ -170,60 +120,47 @@ void processTouchInputs()
         {
             if (lastState[sensor] == TOUCHED)
             {
-                 if ((millis() - lastTimePressed[sensor]) > LONG_TOUCH_DURATION_MS)
-                 {
-                     if (touchSensorsEvents[sensor] == NO_EVENT)
-                     {
-                         touchSensorsEvents[sensor] = LONG_TOUCH_EVENT;
-                         MWST_ToggleIncreaseBrightness(sensor);
-                         SerialBT.println("Long " + String(sensor));
-                         // longClickDetectedHandler(touchSensorTypes[sensor]);
-                     }
-                     else if (touchSensorsEvents[sensor] == LONG_TOUCH_EVENT)
-                     {
-                         MWST_ToggleIncreaseBrightness(sensor);
-                     }
-                 }
+                if ((millis() - lastTimePressed[sensor]) > LONG_TOUCH_DURATION_MS)
+                {
+                    if (touchSensorsEvents[sensor] == NO_EVENT)
+                    {
+                        touchSensorsEvents[sensor] = LONG_TOUCH_EVENT;
+                        Serial.println("Long " + String(sensor));
+                    }
+                    else if (touchSensorsEvents[sensor] == LONG_TOUCH_EVENT)
+                    {
+                        MWST_IncreaseStripIlumination(sensor, 1);
+                    }
+                }
             }
             else
             {
-
-                clickHandler(touchSensorTypes[sensor]);
                 lastState[sensor] = TOUCHED;
                 lastTimePressed[sensor] = millis();
-                // longClickHandler(stouchSensorTypes[sensor]);
             }
-            /* if (touchSensorsEvents[sensor] == NOT_TOUCHED && (millis() - lastTimePressed[sensor]) > DEBOUNCE_TIME)
-              {
-                  lastState[sensor] = TOUCHED;
-                  touchSensorsEvents[sensor] = TOUCHED;
-                  Serial.println("Touched " + String(SENSOR_NAMES[sensor]));
-                  clickHandler(touchSensorTypes[sensor]);
-              }*/
         }
         else
         {
             if (lastState[sensor] == TOUCHED)
             {
-                SerialBT.println("Touched " + String(sensor));
-                lastState[sensor] = NOT_TOUCHED;
-                touchSensorsEvents[sensor] == NO_EVENT;
-                /*
-            if (touchSensorsEvents[sensor] != LONG_TOUCH_EVENT)
-            {
                 Serial.println("Touched " + String(sensor));
-                clickHandler(touchSensorTypes[sensor]);
+                if (touchSensorsEvents[sensor] != LONG_TOUCH_EVENT)
+                {
+                    clickHandler(touchSensorTypes[sensor]);
+                }
+                else
+                {
+                    MWST_ToggleIncreaseBrightness(sensor);
+                    Serial.println("Toggling Brightness " + String(sensor));
+                }
                 lastState[sensor] = NOT_TOUCHED;
-                touchSensorsEvents[sensor] == NO_EVENT;
-            } */
+                touchSensorsEvents[sensor] = NO_EVENT;
             }
-            
             else
             {
                 touchSensorsEvents[sensor] = NO_EVENT;
-                lastState[sensor] == NOT_TOUCHED;
+                lastState[sensor] = NOT_TOUCHED;
             }
-            //Serial.println("Release " + String(sensor));
         }
     }
 }
@@ -235,27 +172,17 @@ void HMIM_Initialize()
     touch_pad_set_voltage(TOUCH_HVOLT_2V7, TOUCH_LVOLT_0V5, TOUCH_HVOLT_ATTEN_0V);
 
     // Set measuring time for the Touch Sensor FSM to improve resolution for bigger capacitive surface
-    touch_pad_set_meas_time(50, 7000);
+   // touch_pad_set_meas_time(50, 7000);
     touch_pad_filter_start(TOUCHPAD_FILTER_TOUCH_PERIOD);
-    // touch_pad_filter_stop();
-    // touch_pad_filter_delete();
 
     uint16_t touchValue = 0;
-    uint16_t sample;
     for (uint8_t sensor = 0; sensor < MAX_TOUCH_SENSORS; sensor++)
     {
         touch_pad_config(sensorsInput[sensor], TOUCH_THRESH_NO_USE);
 
         delay(100); // Wait for configuration to settle
 
-        for (uint8_t i = 0; i < 5; i++)
-        {
-            touch_pad_read_filtered(sensorsInput[sensor], &sample);
-           // touch_pad_read_raw_data(sensorsInput[sensor], &sample);
-            touchValue += sample;
-        }
-
-        // touch_pad_read_filtered(sensorsInput[sensor], &touchValue);
+        touch_pad_read_filtered(sensorsInput[sensor], &touchValue);
 
         touchThresholds[sensor] = touchValue * TOUCH_THRESHOLD_PROPORTION;
     }
@@ -329,10 +256,10 @@ void HMIN_ProcessHMI()
     for (uint8_t sensorType = 0; sensorType < (sizeof(touchSensorTypes) / sizeof(touchSensorTypes[0])); sensorType++)
     {
         processTouchInputs();
-        if (touchSensorsEvents[sensorType] == LONG_TOUCH_EVENT)
-        {
-            MWST_IncreaseStripIlumination(sensorType, 1);
-        }
+        /* if (touchSensorsEvents[sensorType] == LONG_TOUCH_EVENT)
+          {
+              MWST_IncreaseStripIlumination(sensorType, 1);
+          }*/
     }
     MWIH_ReadRotaryEncoder();
 }
